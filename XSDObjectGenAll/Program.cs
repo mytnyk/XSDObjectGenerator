d5e55e8b6e1a4b7bea276091469976f272daadf4
Alex Mytnyk <alex.mytnyk@ukr.net>
2019-07-03 08:58:34 +0300

    Added arguments parser

--- XSDObjectGenAll/Program.cs
+++ XSDObjectGenAll/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,56 +11,71 @@ namespace XSDObjectGenAll
 {
     class Program
     {
+        public class Options
+        {
+            [Option('o', "output", Required = false, HelpText = "Output directory")]
+            public string Output { get; set; }
+            [Option('i', "input", Required = true, HelpText = "Input directory")]
+            public string Input { get; set; }
+            [Option('n', "namespace", Required = true, HelpText = "Namespace")]
+            public string Namespace { get; set; }
+            [Option('s', "shell_execute", Required = false, HelpText = "Shell execute")]
+            public bool Shell_execute { get; set; }
+            [Option('c', "clear_directory", Required = false, HelpText = "Clear directory")]
+            public bool Clear_directory { get; set; }
+            [Option('w', "wait_for_exit", Required = false, HelpText = "Wait for exit")]
+            public bool Wait_for_exit { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            if (args == null || (args.Length != 5 && args.Length != 6))
-            {
-                Console.WriteLine("Please run program with args: [path to schemas] [namespace] [use shell execute(y for yes, n for no)] [clear directory(y for yes, n for no)][wait for exit(y for yes, n for no)] [save folder]. If save folder is empty uses exe folder.");
-                Console.ReadKey();
-                return;
-            }
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Clear_directory && o.Output != null)
+                       {
+                           DirectoryInfo di = new DirectoryInfo($"{Environment.CurrentDirectory}\\{o.Output}");
+                           if (di.Exists)
+                           {
+                               foreach (FileInfo file in di.GetFiles())
+                               {
+                                   file.Delete();
+                               }
+                               foreach (DirectoryInfo dir in di.GetDirectories())
+                               {
+                                   dir.Delete(true);
+                               }
+                           }
+                       }
 
+                       string[] filePaths = Directory.GetFiles(o.Input);
+                       foreach (var path in filePaths)
+                       {
+                           System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+                           pProcess.StartInfo.FileName = "XSDObjectGen.exe";
+                           if (o.Output != null)
+                           {
+                               pProcess.StartInfo.Arguments = $"{path} /n:{o.Namespace} /f:{o.Output}\\{Path.GetFileNameWithoutExtension(path)}";
+                               if (!Directory.Exists($"{Environment.CurrentDirectory}\\{o.Output}"))
+                               {
+                                   Directory.CreateDirectory($"{Environment.CurrentDirectory}\\{o.Output}");
+                               }
+                           }
+                           else
+                           {
+                               pProcess.StartInfo.Arguments = $"{path} /n:{o.Namespace} /f:{Path.GetFileNameWithoutExtension(path)}";
+                           }
 
-            if (args[3].ToLower() == "y" || args[3].ToLower() == "yes")
-            {
-                DirectoryInfo di = new DirectoryInfo($"{Environment.CurrentDirectory}\\{args[5]}");
-                foreach (FileInfo file in di.GetFiles())
-                {
-                    file.Delete();
-                }
-                foreach (DirectoryInfo dir in di.GetDirectories())
-                {
-                    dir.Delete(true);
-                }
-            }
-
-            string[] filePaths = Directory.GetFiles(args[0]);
-            foreach (var path in filePaths)
-            {
-                System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
-                pProcess.StartInfo.FileName = "XSDObjectGen.exe";
-                if (args.Length == 6)
-                {
-                    pProcess.StartInfo.Arguments = $"{path} /n:{args[1]} /f:{args[5]}\\{Path.GetFileNameWithoutExtension(path)}";
-                    if (!Directory.Exists($"{Environment.CurrentDirectory}\\{args[5]}"))
-                    {
-                        Directory.CreateDirectory($"{Environment.CurrentDirectory}\\{args[5]}");
-                    }
-                }
-                else
-                {
-                    pProcess.StartInfo.Arguments = $"{path} /n:{args[1]} /f:{Path.GetFileNameWithoutExtension(path)}";
-                }
-                
-                if (args[2].ToLower() == "y" || args[2].ToLower() == "yes")
-                    pProcess.StartInfo.UseShellExecute = true;
-                else
-                    pProcess.StartInfo.UseShellExecute = false;
-                pProcess.Start();
-                if (args[4].ToLower() == "y" || args[4].ToLower() == "yes")
-                    pProcess.WaitForExit();
-                pProcess.Close();
-            }
+                           if (o.Shell_execute)
+                               pProcess.StartInfo.UseShellExecute = true;
+                           else
+                               pProcess.StartInfo.UseShellExecute = false;
+                           pProcess.Start();
+                           if (o.Wait_for_exit)
+                               pProcess.WaitForExit();
+                           pProcess.Close();
+                       }
+                   });
         }
     }
 }
